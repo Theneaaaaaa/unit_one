e27e324c8add8c67afa44835af20d432d6f5217b
Thenea <thenea@iMac4.local>
2018-09-13 12:06:33 -0400

    I finished the project following the rubric this time XD.

--- assignment_1.py
+++ assignment_1.py
@@ -1,3 +1,9 @@
+# Thenea Sun
+# Sept. 13th
+# assignment_one
+# This is my first project about how to use turtle for drawing different graphs, I chose to create a beautiful graph
+# with repeating hexagon and diamond.
+
 import turtle
 
 turtle.speed(75)
@@ -6,21 +12,23 @@ turtle.pencolor('#5DADE2')
 
 turtle.pensize(2)
 
-def makeAHexagon():
+
+def make_a_hexagon():
     for x in range(6):
         turtle.forward(150)
         turtle.left(60)
 
 
 for x in range(20):
-    makeAHexagon()
+    make_a_hexagon()
     turtle.left(18)
 
 turtle.pencolor('#00CCFF')
 
 turtle.pensize(1)
 
-def makeADiamond():
+
+def make_a_diamond():
     for x in range(1):
         turtle.forward(150)
         turtle.left(15)
@@ -34,8 +42,9 @@ def makeADiamond():
         turtle.forward(150)
         turtle.left(165)
 
+
 for x in range(20):
-    makeADiamond()
+    make_a_diamond()
     turtle.left(18)
 
 turtle.exitonclick()
